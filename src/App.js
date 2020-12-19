266e2722757b1a7897adf29350a3832e605ba388
Tayyeb Rashid <tayyebrashidpro@gmail.com>
2020-12-19 12:27:30 +0400

    deleted all previous progress for better format

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from "react";
-import Navbar from "./Navbar";
-import MainHero from "./Home/MainHero";
-import Partners from "./Home/Partners"
-import Process from "./Home/Process"
-import Footer from "./Footer"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+
 function App() {
 	return (
 		<>
-			<Navbar />
-			<main>
-				<MainHero />
-				{/* <Partners/> */}
-				<Process/>
-				<Footer/>
-			</main>
+		<Header/>
+		<main>
+		<h1>Welcome</h1>
+		</main>
+		<Footer/>
 		</>
 	);
 }
