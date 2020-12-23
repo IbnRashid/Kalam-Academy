199a1873fa02e2851545652dabcc7665f9d860f5
Tayyeb Rashid <tayyebrashidpro@gmail.com>
2020-12-23 07:34:38 +0400

    mid-section of home page

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,7 @@
 import React from "react";
 import img from "./main-hero-img.png";
+import p_img from "./favicon.svg";
+
 
 const HomeScreen = () => {
 	return (
@@ -7,9 +9,7 @@ const HomeScreen = () => {
 			<div className="main-hero">
 				<div className="main-hero-container">
 					<div className="main-hero-text">
-						<p className="hero-title">
-							Your Road To Knowledge Starts Here
-						</p>
+						<p className="hero-title">Your Road To Knowledge Starts Here</p>
 						<p>
 							Build your skills with our courses from experts around the world
 						</p>
@@ -21,7 +21,15 @@ const HomeScreen = () => {
 				</div>
 			</div>
 			<div className="process">
-				<h2 className="text-center">Our Process</h2>
+				<h2 className="text-center main-process-text">
+					How Kalam Academy Works
+				</h2>
+				<ul className="main-process">
+					<li><img src={p_img}/>How Kalam Academy Works How Kalam Academy WorksHow Kalam Academy Works</li>
+					<li><img src={p_img}/>How Kalam Academy Works How Kalam Academy WorksHow Kalam Academy Works</li>
+					<li><img src={p_img}/>How Kalam Academy Works How Kalam Academy WorksHow Kalam Academy Works</li>
+					<li><img src={p_img}/>How Kalam Academy Works How Kalam Academy WorksHow Kalam Academy Works</li>
+				</ul>
 			</div>
 		</>
 	);
