e61a8915e13ccb7f1822ae5fd4cec2b9162ed12f
Tayyeb Rashid <tayyebrashidpro@gmail.com>
2021-01-02 14:58:48 +0400

    mostly completed the featured courses

--- src/components/FeaturedCourse.js
+++ src/components/FeaturedCourse.js
@@ -4,11 +4,22 @@ const FeaturedCourse = ({ course }) => {
 	return (
 		<div className=" fc">
 			<img className="fc-img" src={course.image} alt={course.name} />
-			<div className="fc-top">
-				<h5 className="fc-name">{course.name}</h5>
-				{/* <img src={course.profilePic} alt={course.teacher} className="profile-pic"/> */}
-			</div>
+			<h5 className="fc-name">{course.name}</h5>
 			<p>{course.shortDescription}</p>
+			<div className="fc-bottom">
+				<div className="profile">
+					<img
+						src={course.profilePic}
+						alt={course.teacher}
+						className="profile-pic"
+					/>
+					<h5>{course.teacher}</h5>
+				</div>
+				<div className="fc-price">
+					<h5>${course.price}</h5>
+				</div>
+			</div>
+			<div className="fc-button"><h1>TAKE COURSE</h1></div>
 		</div>
 	);
 };
