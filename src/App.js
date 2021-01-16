7d8fa27aa46ed515030d59f6b05bc9cd7a69d744
Tayyeb Rashid <tayyebrashidpro@gmail.com>
2021-01-16 18:39:45 +0400

    new design coming up

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import {BrowserRouter as Router, Route} from "react-router-dom"
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen"
+import Header2 from "./components/Header2";
 
 function App() {
 	return (
 		<Router>
+			<Header2/>
 			<Header />
 			<main>
 				<Route path="/" component={HomeScreen} exact/>
